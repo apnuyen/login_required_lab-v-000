dea6fa82def145c0f19e036e4f8048ff3d982083
An Nuyen <annuyen@gmail.com>
2018-03-13 18:18:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user 
+  helper_method :current_user
 
-  def current_user 
-    session[:name] ||= nil 
-  end 
-  
+  def current_user
+    session[:name] ||= nil
+  end
+
 end
